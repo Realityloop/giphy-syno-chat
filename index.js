8dc5c7fa8b051a6dc3e5b1b100bfb52e699f7a12
Stuart Clark <stu@rtclark.net>
2017-09-13 11:57:34 +1000

    Updated to return a result and work with GET requests.

--- index.js
+++ index.js
@@ -12,13 +12,25 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.get('/', function (req, res) {
-    res.send()
+    if (typeof(req.query.text) !== 'undefined') {
+        var text = req.query.text.substr(7)
+        giphySynoChat.returnResult(text, res)
+    }
 })
 
 app.post('/', function (req, res) {
     if (typeof(req.body.text) !== 'undefined') {
         var text = req.body.text.substr(7)
+        giphySynoChat.returnResult(text, res)
+    }
+})
 
+app.listen(3000, function () {
+    console.log('Server running on http://localhost:3000')
+})
+
+var giphySynoChat = {
+    returnResult: function (text, res) {
         var query = {
             api_key: process.env.GIPHY_API_KEY,
             q: text
@@ -32,7 +44,7 @@ app.post('/', function (req, res) {
 
                 if (typeof(data.data[rand]) !== 'undefined') {
                     var item = data.data[rand]
-                    res.send(item)
+                    res.send({text: item.images.downsized_medium.url})
                 }
 
                 // Error if item isn't present.
@@ -49,8 +61,4 @@ app.post('/', function (req, res) {
             }
         })
     }
-})
-
-app.listen(3000, function () {
-    console.log('Server running on http://localhost:3000')
-})
+}
